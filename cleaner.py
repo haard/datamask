48ad9cc0ba22442db0c2dbd772b06d2940c901d8
Fredrik Håård <fredrik@metallapan.se>
2021-03-01 18:47:52 +0100

    Cleanup and TOOD-sprinkling

--- cleaner.py
+++ cleaner.py
@@ -6,18 +6,21 @@ import random
 from collections import defaultdict
 import argparse
 from concurrent.futures import ThreadPoolExecutor
+import logging
 
-fake = faker.Faker()
-serials = {}
+logging.basicConfig()
+LOG = logging.getLogger(__name__)
+FAKER = faker.Faker()
+SERIALS = {}
 
 
 def serial(name, seed=0):
-    if not name in serials:
-        serials[name] = seed
+    if not name in SERIALS:
+        SERIALS[name] = seed
 
     def cap(_):
-        serials[name] += 1
-        return serials[name]
+        SERIALS[name] += 1
+        return SERIALS[name]
     return cap
 
 
@@ -30,47 +33,48 @@ def get_mapper(table_schema, table_name, column_name, data_type, pii_type, **_):
         c = 'QWERTYUIOPLKJHGFDSAMNBVCXZ'
         return ''.join((random.choice(c) for _ in range(15)))
 
+    # Todod: args and context
     fakers = {
-        'person_firstname': lambda _: fake.first_name(),
-        'person_familyname': lambda _: fake.last_name(),
-        'person_name': lambda _: fake.name(),
+        'person_firstname': lambda _: FAKER.first_name(),
+        'person_familyname': lambda _: FAKER.last_name(),
+        'person_name': lambda _: FAKER.name(),
         'tla': tla,
-        'business_name': lambda _: fake.company(),
-        'slug': lambda _: fake.slug(),
+        'business_name': lambda _: FAKER.company(),
+        'slug': lambda _: FAKER.slug(),
         'null': lambda _: None,
-        'text_short': lambda _: fake.sentence(),
-        'text': lambda _: fake.paragraph(),
-        'email': lambda _: fake.email(domain='example.com'),
-        'user_agent': lambda _: fake.user_agent(),
-        'url': lambda _: fake.uri(),
-        'url_image': lambda _: fake.image_url(),
-        'phonenumber': lambda _: fake.msisdn()[:11],
-        'address': lambda _: fake.address(),
-        'city': lambda _: fake.city(),
-        'zipcode': lambda _: fake.postcode(),
-        'filename': lambda _: fake.file_name(),
-        'inet_addr': lambda _: fake.ipv4(),
+        # TODO: args for bs etc? maxlen!
+        'text_short': lambda _: FAKER.sentence(),
+        'text': lambda _: FAKER.paragraph(),
+        'email': lambda _: FAKER.email(domain='example.com'), # TODO
+        'user_agent': lambda _: FAKER.user_agent(),
+        'url': lambda _: FAKER.uri(),
+        'url_image': lambda _: FAKER.image_url(),
+        'phonenumber': lambda _: FAKER.msisdn()[:11],
+        'address': lambda _: FAKER.address(),
+        'city': lambda _: FAKER.city(),
+        'zipcode': lambda _: FAKER.postcode(),
+        'filename': lambda _: FAKER.file_name(),
+        'inet_addr': lambda _: FAKER.ipv4(),  # TODO private?
         'username': slug,
-        'int': lambda _: random.randint(0, 300000000),
+        'int': lambda _: random.randint(0, 300000000),  # TODO: arg+default
         'password': slug,
-        'serial': serial(f"{table_schema}.{table_name}.{column_name}", 200000000),
+        'serial': serial(f"{table_schema}.{table_name}.{column_name}", 200000000),  # TODO: arg+default instead
 
     }
     return fakers[pii_type]
 
 
 def get_piis(source_file_csv: str):
-    source = csv.DictReader(open(source_file_csv), delimiter=',')
+    source = csv.DictReader(open(source_file_csv), delimiter=',') # TODO: args
     tables: dict = defaultdict(dict)
     for line in source:
         if line['pii'] == 'yes':
             try:
                 mapper = get_mapper(**line)
-
                 tables[f"{line['table_schema']}.{line['table_name']}"][line['column_name']] = (
                     line, mapper)
             except:
-                print(line)
+                LOG.exception(f"Erroring on '{line}'")
                 raise
     return tables
 
@@ -108,15 +112,10 @@ def mask_pii(table, mappers, dsn):
             write_cursor.execute(sql, values)
             assert write_cursor.rowcount == 1
         except Exception as e:
-            print(table)
-            print(e)
-            print('row', row)
-            print(old_values)
-            print(new_values)
-            print('new', new_row)
+            LOG.exception(f"Table: {table}")
             raise
     conn.commit()
-    print(f'{table} commited')
+    LOG.info(f'{table} commited')
 
 def main():
 
@@ -127,23 +126,21 @@ def main():
     args = parser.parse_args()
     executor = ThreadPoolExecutor(max_workers=args.executors)
     tasks = []
-    for table, mappers in get_piis(args.filename).items():
-        print(table)
+    for table, mappers in get_piis(args.filename).items():        
         if mappers:
-            print(f"  masking {', '.join(mappers.keys())}")
+            LOG.info(f"{table}:  masking {', '.join(mappers.keys())}")
             tasks.append(executor.submit(mask_pii, table, mappers, args.dburl))
         else:
-            print('  not masking')
+            LOG.debug(f"{table}: not masking")
 
-    
     completed = 0
     taskcount = len(tasks)
-    print(f"{taskcount} tasks submitted")
+    LOG.debug(f"{taskcount} tasks submitted")
     for task in tasks:
         task.result()
         completed += 1
-        print(f"{completed}/{taskcount}")
-    print('Done')
+        LOG.debug(f"{completed}/{taskcount}")
+    LOG.info('Done')
 
 
 if __name__ == "__main__":
